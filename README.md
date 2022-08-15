8b0d2bfdbbcf714e8379784bec1c488031e5eae9
ivandjoh <ivandi.djoh@gmail.com>
2022-08-16 02:12:25 +0800

    project init

---
+++ README.md
@@ -0,0 +1,5 @@
+## Ivandjoh Micro Spring (Service Registry)
+
+### About the project
+This is a Project for the Service Registry of `Ivandjoh` Micro Spring.
+This Microservice is used to manage the `EUREKA SERVER`.
---
+++ src/test/java/ivandjoh/online/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ivandjoh.online.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
