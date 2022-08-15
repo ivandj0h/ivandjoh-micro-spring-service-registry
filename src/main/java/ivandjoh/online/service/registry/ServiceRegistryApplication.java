870a855600d8307cd985ac2dd08f80007beca22a
ivandjoh <ivandi.djoh@gmail.com>
2022-08-16 03:07:03 +0800

    enable the eureka server

--- src/main/java/ivandjoh/online/service/registry/ServiceRegistryApplication.java
+++ src/main/java/ivandjoh/online/service/registry/ServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package ivandjoh.online.service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
